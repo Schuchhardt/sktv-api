6c5f837adef1a53493e1768ff1c3ad562fee8120
Sebastián Schuchhardt <seba.sch999@gmail.com>
2018-01-10 23:30:50 -0300

    gcloud credentials in env

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,10 @@
 require "google/cloud/storage"
+
 class Event < ActiveRecord::Base
 	has_many :photos, :inverse_of => :event
+	after_create :load_photos
 
-	BASE_URL="https://storage.googleapis.com/sktv/Eventos/"
+	BASE_URL="https://storage.googleapis.com/sktv/"
 
 	def cover_url
 		gen_gcloud_url self.code_name, self.image_url
@@ -14,8 +16,29 @@ class Event < ActiveRecord::Base
 
 	private
 
+	def load_photos
+	
+		storage = Google::Cloud::Storage.new(
+			project_id: ENV["GCLOUD_PROJECT_ID"],
+			credentials: JSON.parse(ENV["GCLOUD_APPLICATION_CREDENTIALS"])
+		)
+		bucket = storage.bucket ENV["GCLOUD_BUCKET"]
+
+		if self.code_name
+			self.photos.delete_all
+			
+			event_files = bucket.files prefix: "Eventos/#{self.code_name}"
+		
+			event_files.all.each do |file|
+				file.acl.public!
+				puts file.name
+				Photo.create(photo_url: "#{BASE_URL}#{file.name}", event_id: self.id)
+			end
+		end
+	end
+
 	def gen_gcloud_url code_name, image_url
-		code_name && image_url ? "#{BASE_URL}#{code_name}/#{image_url}" : 'https://i.imgur.com/hXPwgqu.jpg'
+		code_name && image_url ? "#{BASE_URL}Eventos/#{code_name}/#{image_url}" : 'https://i.imgur.com/hXPwgqu.jpg'
 	end
 
 
--- lib/tasks/gcloud-storage.rake
+++ lib/tasks/gcloud-storage.rake
@@ -5,25 +5,25 @@ namespace :storage do
   desc "Test google storage"
   task :sync_events => :environment do
 
-  	URL="https://storage.googleapis.com/sktv/"
-	
+  	base_url = "https://storage.googleapis.com/sktv/"
+  	puts "***"
+	puts ENV['GCLOUD_APPLICATION_CREDENTIALS'].inspect
 	storage = Google::Cloud::Storage.new(
 		project_id: ENV["GCLOUD_PROJECT_ID"],
-		credentials: "#{Rails.root}/config/#{ENV['GCLOUD_CREDENTIAL_FILE']}"
+		credentials: StringIO.new(ENV["GCLOUD_APPLICATION_CREDENTIALS"])
 	)
 	bucket = storage.bucket ENV["GCLOUD_BUCKET"]
 
-
 	Event.all.each do |event|
-		event.photos.delete_all
 		if event.code_name
+			event.photos.delete_all
 			
 			event_files = bucket.files prefix: "Eventos/#{event.code_name}"
 		
 			event_files.all.each do |file|
 				file.acl.public!
 				puts file.name
-				Photo.create(photo_url: "#{URL}#{file.name}", event_id: event.id)
+				Photo.create(photo_url: "#{base_url}#{file.name}", event_id: event.id)
 			end
 		end
 	end
